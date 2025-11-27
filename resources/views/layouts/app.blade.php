9cae8952ab1ee86593d428f76ce9488bff4d41a9
steven <sletulzo@gmail.com>
2025-11-27 18:53:54 +0100

    new version

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="manifest" href="{{ asset('manifest.json') }}">
+        <link rel="icon" href="{{ Vite::asset('resources/images/logo-only.svg') }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
