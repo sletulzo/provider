ddec53089462ce417f4378970d5e94a46d67ee14
steven <sletulzo@gmail.com>
2026-06-04 23:18:54 +0200

    new version

--- resources/views/order/card.blade.php
+++ resources/views/order/card.blade.php
@@ -6,7 +6,7 @@
         <div class="mobile-card-item-subtitle">{{ $order->provider?->name }} - {{ $order->user?->name }}</div>
     </div>
     <div class="mobile-card-item-right">
-        <span class="f-11">{{ $order->lines()->sum('quantity') }} articles</span>
+        <span class="f-11 nowrap">{{ $order->lines()->sum('quantity') }} articles</span>
     </div>
     <div class="mobile-card-item-status">
         <div class="mobile-card-item-status-item {{ $status['slug'] }}">{{ $status['label'] }}</div>
