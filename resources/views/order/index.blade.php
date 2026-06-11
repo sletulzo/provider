2009ebc34eb30f5369e196020d0fce911aaa400d
steven <sletulzo@gmail.com>
2026-06-11 22:52:26 +0200

    new version

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -1,4 +1,13 @@
 <x-app-layout>
+    <x-slot name="headerActions">
+        <a href="{{ route('indents') }}" class="table-wrapper-action relative">
+            <i class="fa-solid fa-cart-shopping">
+                @if (Auth::user()->tenant && Auth::user()->tenant->countOrdersWaiting())
+                    <span class="bubble">{{ Auth::user()->tenant->countOrdersWaiting() }}</span>
+                @endif
+            </i>
+        </a>
+    </x-slot>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             <i class="fa-solid fa-pencil"></i> Commandes effectuées
